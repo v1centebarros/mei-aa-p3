f4d351adade623e0a5fc7bdf1b3f1038c9ce158b
v1centebarros <vmabarros@ua.pt>
2023-12-29 15:28:39 +0000

    LossyCounting well done

--- src/algorithms.py
+++ src/algorithms.py
@@ -32,53 +32,26 @@ def fixed_probability_counter(text, probability=1 / 32):
 
 
 @benchmark
-def lossy_counting(text, bucket_size=100, epsilon=0.01):
-    current_bucket = 1
+def lossy_counting(text: str, k=None, error: float = None):
     letter_count = defaultdict(int)
-    frequencies = defaultdict(int)
-
-    for i, letter in enumerate(text):
-        if letter.isalpha():
-            letter_count[letter] += 1
-
-            # Check if the current bucket is full
-            if (i + 1) % bucket_size == 0:
-                # Update frequencies and remove infrequent items
-                for letter, count in list(letter_count.items()):
-                    if count + current_bucket * epsilon < current_bucket:
-                        del letter_count[letter]
-                    else:
-                        frequencies[letter] = count
-                current_bucket += 1
-
-    # Include counts from the last bucket if it's not full
-    for letter, count in letter_count.items():
-        frequencies[letter] = max(frequencies[letter], count)
-
-    return dict(frequencies)
-
-
-@benchmark
-def lossy_counting_gigachad(text, k=None, error=0.01, N=10):
-    counter_map = defaultdict(int)
     total = 0
     delta = 0
-    window_width = k if k is not None else ceil(1 / error)
+    bucket_width = k if k is not None else ceil(1 / error)
 
     for letter in text:
         if letter.isalpha():
-            # Increment the count for the letter
-            counter_map[letter] += 1
+            if letter not in letter_count:
+                letter_count[letter] = delta + 1
+            else:
+                letter_count[letter] += 1
             total += 1
 
             # Update counters and remove infrequent items
-            threshold = floor(total / window_width)
+            threshold = floor(total / bucket_width)
             if threshold != delta:
                 delta = threshold
-                for item, count in list(counter_map.items()):
+                for item, count in list(letter_count.items()):
                     if count < delta:
-                        counter_map.pop(item)
+                        letter_count.pop(item)
 
-    # Get the most frequent N items
-    most_frequent = sorted(counter_map.keys(), key=lambda x: counter_map[x], reverse=True)[:N]
-    return {letter: counter_map[letter] for letter in most_frequent}
+    return letter_count
