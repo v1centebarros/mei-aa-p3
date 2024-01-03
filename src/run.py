bdbe61c4d8e8b5597cd7724bbb976c30d7042b5e
v1centebarros <vmabarros@ua.pt>
2024-01-03 02:44:40 +0000

    Split the run logic of each algorithm to be easier to organise the data. Split the main function into 2: one to run the algorithms, other to do the stats

--- src/run.py
+++ src/run.py
@@ -1,31 +1,90 @@
+import json
 import os
+from collections import OrderedDict
 
 FILE_PATH = "../books/"
+RESULTS_FILE = "../results/results.json"
 from algorithms import exact_counter, fixed_probability_counter, lossy_counting
-from utils import log, Stats
+from utils import log, Stats, CounterResult
 
 books = [book for book in os.listdir(FILE_PATH) if book.endswith(".txt")]
 algorithms = [exact_counter, fixed_probability_counter, lossy_counting]
 
 
-def main():
+def sort_result(result, time):
+    sorted_result = sorted(result.items(), key=lambda x: x[1], reverse=True)
+    k = {k: {"result": OrderedDict(sorted_result[:k]), "time": time} for k in [3, 5, 10]}
+    return k
+
+
+def run_exact_counter(text):
+    result, time = exact_counter(text)
+    return sort_result(result, time)
+
+
+def run_fixed_probability_counter(text, runs=100):
+    results = []
+    average_result = {}
+    average_time = 0
+
+    for i in range(runs):
+        log.info(f"Running fixed probability counter {i + 1} of {runs}")
+        result, time = fixed_probability_counter(text)
+        results.append(result)
+        average_time += time
+
+    for letter in results[0]:
+        average_result[letter] = sum(result[letter] for result in results) / len(results)
+
+    return sort_result(average_result, average_time / runs)
+
+
+def run_lossy_counting(text):
+    k = {}
+    for i in [3, 5, 10]:
+        log.info(f"Running lossy counting with k={i}")
+        result, time = lossy_counting(text, i)
+        sorted_result = sorted(result.items(), key=lambda x: x[1], reverse=True)
+        k[i] = {"result": OrderedDict(sorted_result), "time": time}
+
+    return k
+
+
+def marathon():
     results = {}
     for book in books:
         results[book] = {}
         with open(os.path.join(FILE_PATH, book), "r") as f:
             book_text = f.read()
-            for algorithm in algorithms:
-                log.info(f"Running {algorithm.__name__} on {book}")
-                results[book][algorithm.__name__] = algorithm(book_text)
+            log.info(f"Running exact counter on {book}")
+            results[book]["exact_counter"] = run_exact_counter(book_text)
+            log.info(f"Running fixed probability counter on {book}")
+            results[book]["fixed_probability_counter"] = run_fixed_probability_counter(book_text)
+            log.info(f"Running lossy counting on {book}")
+            results[book]["lossy_counting"] = run_lossy_counting(book_text)
 
-    for book in results:
-        exact_result = results[book]["exact_counter"]
-        for algorithm in algorithms[1:]:
-            approx_result = results[book][algorithm.__name__]
-            stats = Stats(exact_result, approx_result)
-            log.info(f"Results for {algorithm.__name__} on {book}")
-            stats.save_results(f"../results/{book}_{algorithm.__name__}.json", "json")
+    json.dump(results, open(RESULTS_FILE, "w"), indent=4, ensure_ascii=False)
+
+
+def run_stats(results):
+    for book in books:
+        for algorithm in ["fixed_probability_counter", "lossy_counting"]:
+            log.info(f"Running statistics on {book} with {algorithm}")
+            for k in ["3", "5", "10"]:
+                exact_counter_obg = CounterResult(results[book]["exact_counter"][k]["result"],
+                                                  results[book]["exact_counter"][k]["time"])
+                counter = CounterResult(results[book][algorithm][k]["result"], results[book][algorithm][k]["time"])
+                stats = Stats(exact_counter_obg, counter)
+                stats.save_results(f"../results/{book}_{algorithm}_{k}.json", _type="json")
 
 
 if __name__ == "__main__":
-    main()
+    if not os.path.exists("../results"):
+        os.mkdir("../results")
+
+    if not os.path.exists(RESULTS_FILE):
+        marathon()
+
+    results = json.load(open(RESULTS_FILE, "r"))
+
+    run_stats(results)
